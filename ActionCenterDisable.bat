bc15c5e181495a872016120bf789a148a527a308
Rinsho <Rinsho@users.noreply.github.com>
2016-12-07 16:20:02 -0600

    Removed two erroneous spaces.

--- ActionCenterDisable.bat
+++ ActionCenterDisable.bat
@@ -7,11 +7,11 @@ SC stop wscsvc
 SC config wscsvc start=disabled
 
 TAKEOWN /F "%SystemRoot%\system32\ActionCenter.dll"
-ICACLS "%SystemRoot%\system32\ActionCenter.dll" /grant :r administrators:F
+ICACLS "%SystemRoot%\system32\ActionCenter.dll" /grant:r administrators:F
 REN "%SystemRoot%\system32\ActionCenter.dll" ActionCenter.bak
 
 TAKEOWN /F "%SystemRoot%\system32\ActionCenterCPL.dll"
-ICACLS "%SystemRoot%\system32\ActionCenterCPL.dll" /grant :r administrators:F
+ICACLS "%SystemRoot%\system32\ActionCenterCPL.dll" /grant:r administrators:F
 REN "%SystemRoot%\system32\ActionCenterCPL.dll" ActionCenterCPL.bak
 
 ECHO Operations completed.
